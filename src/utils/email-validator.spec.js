273dca7067117463e6feb248c16effb27ab34e07
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-03 21:55:51 -0300

    ensure EmailValidator returns false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,6 +1,8 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
@@ -10,5 +12,13 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@email.com')
 
     expect(isEmailValid).toBe(true)
+  })
+
+  it('should return false if validator returns false', async () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+
+    const isEmailValid = sut.isValid('invalid_email_email.com')
+    expect(isEmailValid).toBe(false)
   })
 })
