939b49eb26f04033796e3cb604fe8b86d4547b39
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-08 14:36:24 -0300

    move user creation from individual tests to beforeEach

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -10,13 +10,19 @@ const makeSut = () => {
 }
 
 describe('UpdateAccessToken Repository', () => {
+  let userId
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
     db = await MongoHelper.getDb()
   })
 
   beforeEach(async () => {
-    await db.collection('users').deleteMany()
+    const userModel = db.collection('users')
+    await userModel.deleteMany()
+    const insertedUser = await userModel.insertOne({
+      email: 'test.email@email.com'
+    })
+    userId = insertedUser.ops[0]._id
   })
 
   afterAll(async () => {
@@ -25,36 +31,14 @@ describe('UpdateAccessToken Repository', () => {
 
   it('Should update the user with the given accessToken', async () => {
     const { sut, userModel } = makeSut()
-
-    const userInserted = await userModel.insertOne({
-      email: 'test.email@email.com',
-      name: 'test_user',
-      age: 50,
-      state: 'test_state',
-      password: 'hashed_password'
-    })
-
-    const userId = userInserted.ops[0]._id
     await sut.update(userId, 'valid_token')
-
     const updatedUser = await userModel.findOne({ _id: userId })
-
     expect(updatedUser.accessToken).toBe('valid_token')
   })
 
   it('Should throw if no userModel is provided', async () => {
     const sut = new UpdateAccessTokenRepository()
-
-    const userModel = db.collection('users')
-    const userInserted = await userModel.insertOne({
-      email: 'test.email@email.com',
-      name: 'test_user',
-      age: 50,
-      state: 'test_state',
-      password: 'hashed_password'
-    })
-
-    const promise = sut.update(userInserted.ops[0]._id, 'valid_token')
+    const promise = sut.update(userId, 'valid_token')
 
     expect(promise).rejects.toThrow(new MissingParamError('userModel'))
   })
@@ -63,7 +47,7 @@ describe('UpdateAccessToken Repository', () => {
     const { sut } = makeSut()
 
     expect(sut.update()).rejects.toThrow(new MissingParamError('userId'))
-    expect(sut.update('user_id')).rejects.toThrow(
+    expect(sut.update(userId)).rejects.toThrow(
       new MissingParamError('accessToken')
     )
   })
