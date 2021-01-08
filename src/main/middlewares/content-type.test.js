fe462bf57d6e277b101cdd568aee285eea59e191
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-08 17:47:44 -0300

    ensure setup Content-Type can be especifiedn

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -3,9 +3,23 @@ const request = require('supertest')
 
 describe('Middleware Content-Type', () => {
   it('Should return json content-type as default', async () => {
-    app.get('/test_middleware_content_type', (req, res) => { res.send() })
+    app.get('/test_middleware_content_type', (req, res) => {
+      res.send()
+    })
 
     const response = await request(app).get('/test_middleware_content_type')
     expect(response.headers['content-type']).toMatch(/json/)
+  })
+
+  it('Should return xml content-type if specified', async () => {
+    app.get('/test_middleware_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+
+    const response = await request(app).get(
+      '/test_middleware_content_type_xml'
+    )
+    expect(response.headers['content-type']).toMatch(/xml/)
   })
 })
