2dcc80de5dfc4ac2408d59324d49dae8b0e0e25e
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-08 14:31:11 -0300

    move UpdateAccessTokenRepository to separate file

---
+++ src/infra/repositories/update-access-token-repository.js
@@ -0,0 +1,32 @@
+const MissingParamError = require('../../utils/errors/missing-param-error')
+
+class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, accessToken) {
+    if (!this.userModel) {
+      throw new MissingParamError('userModel')
+    }
+
+    if (!userId) {
+      throw new MissingParamError('userId')
+    }
+
+    if (!accessToken) {
+      throw new MissingParamError('accessToken')
+    }
+
+    await this.userModel.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          accessToken
+        }
+      }
+    )
+  }
+}
+
+module.exports = UpdateAccessTokenRepository
