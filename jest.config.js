fd7cc3765cc0f5e830bf74a296debe3cfc98cafc
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-09 12:07:23 -0300

    add routes file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**/*.js'],
   preset: '@shelf/jest-mongodb',
   watchPathIgnorePatterns: ['globalConfig']
 }
