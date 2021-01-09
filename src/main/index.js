6b7f3f66ad4e3df87c4197372a8bd7f71fb3e0ce
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-09 11:40:39 -0300

    connect to mongodb before app listen

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,10 @@
-const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-const PORT = 5858
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    const PORT = 5858
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+  })
+  .catch(console.error)
